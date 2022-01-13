d256274cb08e3c6ea86f65157effe0d8a4be65e6
lyz508 <thomas7892009@gmail.com>
2022-01-13 15:03:32 +0800

    Vlan-based Segment Routing

---
+++ SDN-Vland-Based-Segment-Routing/vlanbasedsr/src/main/java/nctu/winlab/vlanbasedsr/NameConfig.java
@@ -0,0 +1,44 @@
+    /*
+ * Copyright 2020-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nctu.winlab.vlanbasedsr;
+
+import org.onosproject.core.ApplicationId;
+import org.onosproject.net.config.Config;
+
+public class NameConfig extends Config<ApplicationId> {
+
+  public static final String edgeSwitch = "edgeSwitch";
+  public static final String switches = "Switch";
+  public static final String segmentId = "SegmentID";
+  public static final String edgeSwitchSubnet = "edgeSwitchSubnet";
+
+  // get specified property as a string
+  public String getEdgeSwitch(){
+    return get(edgeSwitch, null);
+  }
+
+  public String getEdgeSwitchSubnet(){
+    return get(edgeSwitchSubnet, null);
+  }
+
+  public String getSwitches(){
+    return get(switches, null);
+  }
+
+  public String getSegmentId(){
+    return get(segmentId, null);
+  }
+}
